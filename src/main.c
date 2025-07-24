6e9e5b6e486fc62d09b5c1ae181b0e5187db0adc
eliasmurcray <eliasmurcray@gmail.com>
2025-07-23 20:45:40 -0700

    Add partial byte tests

--- src/main.c
+++ src/main.c
@@ -24,10 +24,44 @@ void test_roundtrip_single_byte(void) {
   free(result);
 }
 
+void test_roundtrip_split_bytes(void) {
+  struct BitWriter *writer = bitwriter_malloc();
+  uint8_t data_a[] = {0x2A, 0xAA};  // 0x2AAA (0010101010101010)
+  struct ByteArray arr_a = {data_a, 2};
+  bitwriter_write(writer, &arr_a, 14);  // Buffer: (00101010 101010_00)
+  uint8_t data_b[] = {0xA8};            // 0xA8 (10101000)
+  struct ByteArray arr_b = {data_b, 1};
+  bitwriter_write(writer, &arr_b, 6);  // Buffer: (00101010 10101010 1010_0000)
+
+  const struct ByteArray *written = bitwriter_bytearray(writer);
+  struct BitReader *reader = bitreader_malloc((struct ByteArray *)written);
+  struct ByteArray *read_a = bitreader_read(reader, 14);
+  TEST_ASSERT_NOT_NULL(read_a);
+  TEST_ASSERT_EQUAL(2, read_a->len);
+  TEST_ASSERT_EQUAL_HEX8(0x2A, read_a->data[0]);  // Expected: 00101010
+  TEST_ASSERT_EQUAL_HEX8(0x2A, read_a->data[1]);  // Expected: 00_101010
+
+  // Buffer: 00101010 101010*10 1010_0000
+
+  struct ByteArray *read_b = bitreader_read(reader, 6);
+  TEST_ASSERT_NOT_NULL(read_b);
+  TEST_ASSERT_EQUAL(1, read_b->len);
+  TEST_ASSERT_EQUAL_HEX8(0x2A,
+                         read_b->data[0]);  // Expected: 00_101010 Was 000000_10
+
+  bitwriter_free(writer);
+  bitreader_free(reader);
+  free(read_a->data);
+  free(read_a);
+  free(read_b->data);
+  free(read_b);
+}
+
 int main(void) {
   UNITY_BEGIN();
 
   RUN_TEST(test_roundtrip_single_byte);
+  RUN_TEST(test_roundtrip_split_bytes);
 
   return UNITY_END();
 }
